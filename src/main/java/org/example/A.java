908618a2a5b457204e570f4459885cfb33daf4e7
Nathan Rous <ndrous2084@gmail.com>
2025-06-29 23:32:41 -0400

    shitbob

---
+++ src/main/java/org/example/A.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class A {
+    B b;
+    public A(){}
+    public A(B b){
+        this.b = b;
+        System.out.println("b deez nuts!");
+    }
+}
---
+++ src/main/java/org/example/B.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class B {
+    public B(){}
+
+    public void DeezNuts(){
+        System.out.println("Deez");
+    }
+}
